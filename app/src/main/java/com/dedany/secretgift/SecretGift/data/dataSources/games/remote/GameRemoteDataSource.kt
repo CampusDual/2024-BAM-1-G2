1955061b141dfe3373d5ac343b0017f3e5901731
Daniel Pérez Álvarez <danylerez@gmail.com>
2025-02-04 13:59:06 +0100

    fix

--- app/src/main/java/com/dedany/secretgift/SecretGift/data/dataSources/games/remote/GameRemoteDataSource.kt
+++ app/src/main/java/com/dedany/secretgift/SecretGift/data/dataSources/games/remote/GameRemoteDataSource.kt
@@ -3,5 +3,6 @@ package com.example.baseproject.data.dataSources.games.remote
 import com.example.baseproject.data.dataSources.games.remote.dto.GameDto
 
 interface GameRemoteDataSource {
+    
     suspend fun getGames(): List<GameDto>
 }
